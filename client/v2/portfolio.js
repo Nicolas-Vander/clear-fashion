a448a984503a7a78e42b4428f73c45b96c29d290
Nicolas Vanderstigel <n.vds78@gmail.com>
2022-03-31 17:23:16 +0200

    Adding seach by brand and pagination

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -10,6 +10,7 @@ console.log(body);
 // current products on the page
 let currentProducts = [];
 let currentPagination = {};
+let currentBrand = 'all';
 
 // all my needed var
 let brand = '';
@@ -38,14 +39,14 @@ const setCurrentProducts = ({result, meta}) => {
 /**
  * Fetch products from api
  * @param  {Number}  [page=1] - current page to fetch
- * @param  {Number}  [size=12] - size of the page
+ * @param  {Number}  [limit=12] - size of the page
  * @return {Object}
  */
-const fetchProducts = async (page = 1, size = 12, brand = '') => {
+const fetchProducts = async (page = 1, limit = 12, brand = '') => {
   try {
     const response = await fetch(
       //`https://clear-fashion-api.vercel.app?page=${page}&size=${size}&brand=${brand}`
-      `https://clear-fashion-server-psi.vercel.app/products/search?page=${page}&limit=${size}&brand=${brand}`
+      `https://clear-fashion-server-psi.vercel.app/products/search?page=${page}&limit=${limit}&brand=${brand}`
     );
     const body = await response.json();
 
